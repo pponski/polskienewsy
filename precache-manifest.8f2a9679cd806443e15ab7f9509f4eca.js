f7020698fa595aa81471343bec0d111bdf27ca01
Piotrek Poński <pponski@outlook.com>
2020-10-12 12:06:48 +0200

    Updates

---
+++ precache-manifest.8f2a9679cd806443e15ab7f9509f4eca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9964de256160997981afd735f110ad15",
+    "url": "/polskienewsy/index.html"
+  },
+  {
+    "revision": "42e11bfe9313bd2fe939",
+    "url": "/polskienewsy/static/css/main.a9577e2d.chunk.css"
+  },
+  {
+    "revision": "b4a016b71b2ec2a51ef4",
+    "url": "/polskienewsy/static/js/2.1eda5610.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/polskienewsy/static/js/2.1eda5610.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42e11bfe9313bd2fe939",
+    "url": "/polskienewsy/static/js/main.05e03d16.chunk.js"
+  },
+  {
+    "revision": "c34089520515208e38a5",
+    "url": "/polskienewsy/static/js/runtime-main.66461793.js"
+  },
+  {
+    "revision": "717bfd832348a32336fbed3df7eff967",
+    "url": "/polskienewsy/static/media/article.717bfd83.jpg"
+  }
+]);
